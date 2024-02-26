26e0bb3d7b6b6455c7805259ee3f9619e1ddb74b
neverstopcreating <karolina.uskure@inbox.lv>
2024-02-27 00:28:26 +0200

    card component finished

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,21 @@
 import "@mantine/core/styles.css";
-
-import { MantineProvider } from "@mantine/core";
+import { AppShell, Group, MantineProvider, Text } from "@mantine/core";
+import { DevicesPage } from "@/App/Devices/DevicesPage.tsx";
 
 export function App() {
   return (
     <MantineProvider>
-      <div>hello</div>
+      <AppShell header={{ height: 60 }} padding="md">
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Text size="xl">Devices</Text>
+          </Group>
+        </AppShell.Header>
+
+        <AppShell.Main>
+          <DevicesPage />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
